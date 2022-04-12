ebac0bd35505dcff1bcdaabe560835fb01b4359d
FreeTurk - Emir <teknolojigamers@gmail.com>
2022-04-13 02:26:37 +0300

    make it better

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-	"strconv"
+	"os/exec"
 	"strings"
+	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/fatih/color"
 )
 
@@ -15,48 +18,58 @@ func check(e error) {
 	}
 }
 
-func main() {
-	error := color.New(color.FgRed, color.Bold)
-	file, err := os.Open(os.Args[1])
-	check(err)
-	defer file.Close()
-	out, err := os.Create("OUTPUT.go")
-	check(err)
-
-	out.WriteString("package main\nimport (\n\"fmt\"\n)\nfunc main() {\n")
-	defer out.Close()
-	scanner := bufio.NewScanner(file)
-	line := 1
-	for scanner.Scan() {
-		text := scanner.Text()
-		keyword := strings.SplitN(text, " ", 2)
-		if scanner.Text() == "" {
-			out.WriteString("\n")
-		} else if keyword[0] == "chkpol" {
-			data := strings.Split(keyword[1], " = ")
-			out.WriteString("var " + data[0] + " bool\n" + "if " + data[1] + " % 2 == 0 {\n" + data[0] + " = true\n} else {\n" + data[0] + " = false\n}")
-		} else if keyword[0] == "iter" {
-			varname := strings.Split(keyword[1], " = ")
-			data := strings.Split(varname[1], ", ")
-			out.WriteString("var " + varname[0] + " []int\n")
-			out.WriteString("for i := " + data[3] + "; i <= " + data[4] + "; i++ {\n" + "for n := " + data[0] + "; n <= " + data[1] + "; n++ {\nresult := n " + data[2] + " i\n" + varname[0] + " = append(" + varname[0] + ", result)}}\n")
-		} else if keyword[0] == "var" {
-			data := strings.Split(keyword[1], " = ")
-			out.WriteString(data[0] + " := " + data[1] + "\n")
-		} else if keyword[0] == "print" {
-			data := keyword[1]
-			out.WriteString("fmt.Println(" + data + ")\n")
-		} else if keyword[0] == "//" {
-			out.WriteString(scanner.Text() + "\n")
-		} else {
-			error.Println("An error occured at line " + strconv.Itoa(line) + ". You probably used an undefined keyword.\n" + ">>> " + scanner.Text())
-			os.Remove("OUTPUT.go")
-			out.Close()
-			break
-		}
-		line++
+func preinit(line string, linenum int) {
+	errortext := color.New(color.FgRed).Add(color.Bold).SprintFunc()
+	erroremp := color.New(color.FgRed).Add(color.Underline).SprintFunc()
+	keywords := map[string]bool{"iter": true, "var": true, "//": true, "print": true, "chkpol": true, "": true}
+	words := strings.Split(line, " ")
+	if !keywords[words[0]] {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+		fmt.Println(errortext("\nUnknown keyword used at line ", linenum, "\n", ">>>"), erroremp(words[0]), strings.Join(words[1:], " ")+"\n")
+		os.Exit(0)
 	}
+}
 
-	out.WriteString("}")
-	out.Close()
+func main() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+	successtext := color.New(color.FgGreen).Add(color.Bold).SprintFunc()
+	// get an argument
+	if len(os.Args) != 2 {
+		fmt.Println("Please input a file: matt FILENAME.mtt")
+		os.Exit(0)
+	}
+	file := os.Args[1]
+
+	// open the file
+	f, err := os.Open(file)
+	check(err)
+
+	// read the file line by line
+	scanner := bufio.NewScanner(f)
+	i := 0
+	spinnerd := spinner.New(spinner.CharSets[7], 100*time.Millisecond)
+	spinnerd.Prefix = "Checking for errors "
+	spinnerd.Start()
+	time.Sleep(400 * time.Millisecond)
+	for scanner.Scan() {
+		i += 1
+		preinit(scanner.Text(), i)
+	}
+	spinnerd.Stop()
+
+	fmt.Println(successtext("\nNo errors found!\n"))
+	spinnerd = spinner.New(spinner.CharSets[7], 100*time.Millisecond)
+	spinnerd.Prefix = "Initializing "
+	spinnerd.Start()
+	time.Sleep(400 * time.Millisecond)
+
+	spinnerd.Stop()
+
+	// close the file
+	err = f.Close()
+	check(err)
 }
